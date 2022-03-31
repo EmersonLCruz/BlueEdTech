0d81295a564f52adf139d4a4837187f0c7191919
Emerson Lima <91572990+EmersonLCruz@users.noreply.github.com>
2022-03-31 10:53:04 -0300

    V1.2

--- Modulo 01/Projeto/projeto_iris.py
+++ Modulo 01/Projeto/projeto_iris.py
@@ -1,4 +1,3 @@
-
 from sklearn import datasets
 import pandas as pd
 import numpy as np
@@ -6,135 +5,114 @@ import numpy as np
 iris = datasets.load_iris()
 dados = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['target'])
 
-
-
-
-cont_setosa = 0
-cont_versicolor =0
-cont_virginica = 0
-cont = 0
 target2 = []
+target3 = []
 
 
 def arvore_modelo01(sl,sw,pl,pw):
-    global cont_setosa
-    global cont_versicolor
-    global cont_virginica
     global target2
     if pl <= 2.45:
         print('setosa')
-        cont_setosa += 1
         target2.append(0.0)
     else:
         if pw <= 1.75:
             if pl <= 4.95:
                 if pw <= 1.65:
                     print('versicolor')
-                    cont_versicolor += 1
                     target2.append(1.0)
                 else:
                     print('virginica')
-                    cont_virginica += 1
                     target2.append(2.0)
             else:
                 if pw <= 1.55:
                     print('virginica')
-                    cont_virginica += 1
                     target2.append(2.0)
                 else:
                     print('versicolor')
-                    cont_versicolor += 1
                     target2.append(1.0)
         else:
             if pl <= 4.85:
                 if sw <= 3.1:
                     print('virginica')
-                    cont_virginica += 1
                     target2.append(2.0)
                 else:
                     print('versicolor')
-                    cont_versicolor += 1
                     target2.append(1.0)
             else:
                 print('virginica')
-                cont_virginica += 1
                 target2.append(2.0)
     
 
 
 def arvore_modelo02(sl,sw,pl,pw):
-    global cont_setosa
-    global cont_versicolor
-    global cont_virginica
-    global target2
+    global target3
     if pl <= 2.45:
         print('setosa')
-        cont_setosa += 1
-        target2.append(0.0)
+        target3.append(0.0)
     else:
         if pw <= 1.75:
             print('versicolor')
-            cont_versicolor += 1
-            target2.append(1.0)
+            target3.append(1.0)
         else:
             print('virginica')
-            cont_virginica += 1
-            target2.append(2.0)
+            target3.append(2.0)
+
+def acuracia(acerto,tamanho_amostra):
+    accuracy = acerto/tamanho_amostra
+    return accuracy
 
 
-
+print("="*30)
+print("MODELO 01".center(30))
+print("="*30)
 for i in dados.index:
     arvore_modelo01(dados['sepal length (cm)'][i],dados['sepal width (cm)'][i],dados['petal length (cm)'][i],dados['petal width (cm)'][i])
-
+print("="*30)
 dados['target2'] = target2
 #dados.to_excel('Blue\BlueEdTech\iris02.xlsx')
-
 tamanho_rows = len(dados.index)
-
-
-
+cont = 0
+print("="*30)
+print("MODELO 02".center(30))
+print("="*30)
 while cont < tamanho_rows:
     arvore_modelo02(dados['sepal length (cm)'][cont],dados['sepal width (cm)'][cont],dados['petal length (cm)'][cont],dados['petal width (cm)'][cont])
     cont += 1
 
-dados['target2'] = target2
+dados['target3'] = target3
 #dados.to_excel('Blue\BlueEdTech\iris03.xlsx')
-quant_setosa,quant_versicolor,quant_virgeinica = 0,0,0
-
+print("="*100)
+print("ANALISE MODELO 01".center(100))
+print("="*100)
+acertos = erros = 0
 for i in dados.index:
-    
-    if dados['target'][i] == 0.0:
-        quant_setosa += 1
-        
-    elif dados['target'][i] == 1.0:
-        quant_versicolor += 1
+    if dados['target'][i] == dados['target2'][i]:
+        acertos += 1
     else:
-        quant_virgeinica += 1
-
+        erros += 1
+print(f"O modelo utilizado acertou: {acertos} iris em sua classificação")
+print("Abaixo segue tabela de erros")
 filtro = dados['target'] != dados['target2']
 filtro = dados[filtro]
 print(filtro)
-print(f'Setosas {cont_setosa}')
-print(f'Versicilor {cont_versicolor}')
-print(f'Virginica {cont_virginica}')
 
-'''def acuracia(setosa,versicolor,virginica):
-    if setosa > quant_setosa:
-        setosa = quant_setosa
-    if versicolor > quant_versicolor:
-        versicolor = quant_versicolor
-    if virginica > quant_virgeinica:
-        virginica = quant_virgeinica
-    accuracy = (setosa+versicolor+virginica)/tamanho_rows
-    return accuracy
+ac = acuracia(acertos,tamanho_rows)
+print(f"Accuracy: {ac}")
+print("="*100)
+print("ANALISE MODELO 02".center(100))
+print("="*100)
+acertos = erros = 0
+for i in dados.index:
+    if dados['target'][i] == dados['target3'][i]:
+        acertos += 1
+    else:
+        erros += 1
+print(f"O modelo utilizado acertou: {acertos} iris em sua classificação")
+print("Abaixo segue tabela de erros")
+filtro2 = dados['target'] != dados['target3']
+filtro2 = dados[filtro2]
+print(filtro2)
 
-
-ac = acuracia(cont_setosa,cont_versicolor,cont_virginica)
-print(ac)'''
-
-
-
-
-
-
-
+ac = acuracia(acertos,tamanho_rows)
+print(f"Accuracy: {ac}")
+print("="*100)
--- Modulo 01/codlab05exe02.py
+++ Modulo 01/codlab05exe02.py
@@ -20,17 +20,20 @@ import pandas as pd
 arquivo = pd.read_excel('Modulo 01\datasets\populacao-e-pib-por-estados.xlsx')
 df = pd.DataFrame(arquivo)
 
-#print(df[['Sigla','População']])
+print("="*60)
+print(" POPULAÇÃO POR ESTADOS".center(60))
+print("="*60)
+print(df[['Sigla','População']])
 
 
 # Verificar Estado com maior população
-'''index_maior_populacao = df['População'].idxmax()
+index_maior_populacao = df['População'].idxmax()
 print(f"O estado com maior população é {df.iloc[index_maior_populacao]['Estado']}, com {df.iloc[index_maior_populacao]['População']} de habitantes.")
-print(df.iloc[index_maior_populacao]['Estado'] ,  df.iloc[index_maior_populacao]['População'])'''
+
 
 # Qual a quantidade de Estados
-#print(len(df.index)-1)
-
+print(f"A quantidades de Estados brasileiros é de {len(df.index)-1} + o Distrito Federal")
+print("="*60)
 # Criar novos DataFrames
 norte = df['Região'] == 'Norte'
 norte = df[norte]
@@ -48,24 +51,55 @@ sul = df['Região'] == 'Sul'
 sul = df[sul]
 
 # Calcular números de Estados por região
-#print(f"Norte: {len(norte.index)}")
-#print(f"Nordeste: {len(nordeste.index)}")
-'''print(f"Centro-Oeste: {len(centro_oeste.index)-1}")
+print("NUMERO DE ESTADOS POR REGIÃO".center(60))
+print("="*60)
+print(f"Norte: {len(norte.index)}")
+print(f"Nordeste: {len(nordeste.index)}")
+print(f"Centro-Oeste: {len(centro_oeste.index)-1} + Distrito Federal")
 print(f"Sudeste: {len(sudeste.index)}")
 print(f"Sul: {len(sul.index)}")
-
+print("="*60)
 # Polulação total de cada região
+print("POPULAÇÃO TOTAL POR REGIÃO".center(60))
+print("="*60)
 print(f"Norte: {norte['População'].sum()}")
 print(f"Nordeste: {nordeste['População'].sum()}")
 print(f"Centro-Oeste: {centro_oeste['População'].sum()}")
 print(f"Sudeste: {sudeste['População'].sum()}")
-print(f"Sul: {sul['População'].sum()}")'''
+print(f"Sul: {sul['População'].sum()}")
 
 # Qual região com a maior população?
-if sudeste['População'].sum() > nordeste['População'].sum():
-    print(sudeste['Região'].unique(), ": ",sudeste['População'].sum())
-
+if norte['População'].sum() > nordeste['População'].sum() and norte['População'].sum() > centro_oeste['População'].sum() and norte['População'].sum() > sudeste['População'].sum() and norte['População'].sum() > sul['População'].sum():
+    print(f"A Região com maior população é {norte['Região'].unique().item()} com {norte['População'].sum()} habitantes")
+elif nordeste['População'].sum() > norte['População'].sum() and nordeste['População'].sum() > centro_oeste['População'].sum() and nordeste['População'].sum() > sudeste['População'].sum() and nordeste['População'].sum() > sul['População'].sum():
+    print(f"A Região com maior população é {nordeste['Região'].unique().item()} com {nordeste['População'].sum()} habitantes")
+elif centro_oeste['População'].sum() > norte['População'].sum() and centro_oeste['População'].sum() > nordeste['População'].sum() and centro_oeste['População'].sum() > sudeste['População'].sum() and centro_oeste['População'].sum() > sul['População'].sum():
+    print(f"A Região com maior população é {centro_oeste['Região'].unique().item()} com {centro_oeste['População'].sum()} habitantes")
+elif sudeste['População'].sum() > norte['População'].sum() and sudeste['População'].sum() > centro_oeste['População'].sum() and sudeste['População'].sum() > nordeste['População'].sum() and sudeste['População'].sum() > sul['População'].sum():
+    print(f"A Região com maior população é {sudeste['Região'].unique().item()} com {sudeste['População'].sum()} habitantes")
+else:
+    print(f"A Região com maior população é {sul['Região'].unique().item()} com {sul['População'].sum()} habitantes")
 
 
 # Qual a regiçao com a menor população?
+if norte['População'].sum() < nordeste['População'].sum() and norte['População'].sum() < centro_oeste['População'].sum() and norte['População'].sum() < sudeste['População'].sum() and norte['População'].sum() < sul['População'].sum():
+    print(f"A Região com menor população é {norte['Região'].unique().item()} com {norte['População'].sum()} habitantes")
+elif nordeste['População'].sum() < norte['População'].sum() and nordeste['População'].sum() < centro_oeste['População'].sum() and nordeste['População'].sum() < sudeste['População'].sum() and nordeste['População'].sum() < sul['População'].sum():
+    print(f"A Região com menor população é {nordeste['Região'].unique().item()} com {nordeste['População'].sum()} habitantes")
+elif centro_oeste['População'].sum() < norte['População'].sum() and centro_oeste['População'].sum() < nordeste['População'].sum() and centro_oeste['População'].sum() < sudeste['População'].sum() and centro_oeste['População'].sum() < sul['População'].sum():
+    print(f"A Região com menor população é {centro_oeste['Região'].unique().item()} com {centro_oeste['População'].sum()} habitantes")
+elif sudeste['População'].sum() < norte['População'].sum() and sudeste['População'].sum() < centro_oeste['População'].sum() and sudeste['População'].sum() < nordeste['População'].sum() and sudeste['População'].sum() < sul['População'].sum():
+    print(f"A Região com menor população é {sudeste['Região'].unique().item()} com {sudeste['População'].sum()} habitantes")
+else:
+    print(f"A Região com menor população é {sul['Região'].unique().item()} com {sul['População'].sum()} habitantes")
+print("="*60)
+
 # Qual a população média de cada região?
+print("POPULAÇÃO MÉDIA POR REGIÃO".center(60))
+print("="*60)
+print(f"Norte: {norte['População'].sum() // len(norte.index)}")
+print(f"Nordeste: {nordeste['População'].sum() // len(nordeste.index)}")
+print(f"Centro-Oeste: {centro_oeste['População'].sum() // len(centro_oeste.index)}")
+print(f"Sudeste: {sudeste['População'].sum() // len(sudeste.index)}")
+print(f"Norte: {sul['População'].sum() // len(sul.index)}")
+print("="*60)
